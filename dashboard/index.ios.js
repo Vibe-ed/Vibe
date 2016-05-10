b9e558d343831b1d217cc6fee7476832d0ed3373
Henry Neeb <hneeb@stanford.edu>
2016-05-10 15:00:22 -0700

    Repurposed app for students. Chart at bottom of screen.

--- dashboard/index.ios.js
+++ dashboard/index.ios.js
@@ -1,7 +1,31 @@
-import React, {AppRegistry, StyleSheet, View, Component, Text } from 'react-native';
+/* NOTES
+Have API structure setup, but they are not used.
+
+*/
+
+/* Libraries */
+import React, {
+  AppRegistry,
+  Component,
+  Image,
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+  Animated
+} from 'react-native';
 import RNChart from 'react-native-chart';
 
-const styles = StyleSheet.create({
+/* API Information */
+/* NOT USED - PLACE HOLDER */
+var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+var PAGE_SIZE = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+var REQUEST_URL = API_URL + PARAMS;
+
+/* Styles */
+var styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
@@ -10,47 +34,137 @@ const styles = StyleSheet.create({
     },
     chart: {
         position: 'absolute',
-        top: 16,
+        //top: 16,
+        height: 200,
         left: 4,
         bottom: 4,
         right: 16,
     }
+//    image: {
+//        flex: 1,
+//        transform: [                        // `transform` is an ordered array
+//          {scale: this.state.bounceValue},  // Map `bounceValue` to `scale`
+//      ]
+//    }
 });
 
-const chartData = [
+var chartAPI = {
+    users: ['henry', 'yael', 'tyler'],
+    proportions: [0.01, 0.98, 0.01],
+    commLevel: 3
+};
+
+var chartData = [
     {
         name: 'BarChart',
         type: 'bar',
         color:'purple',
         widthPercent: 0.6,
-        data: [30, 1, 1, 2, 3, 5, 21, 13, 21, 34, 55, 30],
-    },
-    {
-        name: 'LineChart',
-        color: 'gray',
-        lineWidth: 2,
-        highlightIndices: [1, 2],   // The data points at indexes 1 and 2 will be orange
-        highlightColor: 'orange',
-        showDataPoint: true,
-        data: [10, 12, 14, 25, 31, 52, 41, 31, 52, 66, 22, 11],
+        data: chartAPI.proportions,
     }
 ];
 
-const xLabels = ['0','1','2','3','4','5','6','7','8','9','10','11'];
+var xLabels = chartAPI.users
 
-/* Apparently this NEEDS to be called by the name on the folder */
-class dashboard extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <RNChart style={styles.chart}
-                    chartData={chartData}
-                    verticalGridStep={5}
-                    xLabels={xLabels}
-                 />
-            </View>
-        );
-    }
+class dashboard extends React.Component {
+  /* Inital state when loading data */
+  constructor(props: any) {
+    super(props);
+    this.state = {
+//      bounceValue: new Animated.Value(0), // animation
+      graph: null                         // graph
+    };
+  }
+
+  /* Fetches the data exactly once after component finishes loading */
+//  componentDidMount() {
+    /* Fetch chart data */
+//    this.fetchData();
+    /* Animate image */
+//    this.state.bounceValue.setValue(1.5);     // Start large
+//    Animated.spring(                          // Base: spring, decay, timing
+//      this.state.bounceValue,                 // Animate `bounceValue`
+//      {
+//        toValue: 0.8,                         // Animate to smaller size
+//        friction: 1,                          // Bouncier spring
+//      }
+//    ).start();                                // Start the animation
+//  }
+
+
+  /* NOT NEEDED YET */
+  /* Function to actually fetch data */
+  /*
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          graph: responseData.movies,
+        });
+      })
+      .done();
+  } */
+
+  /* Master render function. Will render image or loading screen if
+     trouble pinging server. */
+  render() {
+    /* Dislpays loading text if data not loaded */
+    /* NOT NEEDED
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    } */
+    return (
+      <View style={styles.container}>
+        <RNChart style={styles.chart}
+                 chartData={chartData}
+                 verticalGridStep={5}
+                 xLabels={xLabels}
+             />
+        </View>
+    );
+
+
+//    return (
+//      this.renderGraph
+//      this.renderImage
+//    );
+  }
+
+  /* Renders loading text */
+  /* CURRENTLY NOT USED */
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading data...
+        </Text>
+      </View>
+    );
+  }
+
+  /* Renders the graph */
+  renderGraph() {
+    return (
+      <View style={styles.container}>
+        <RNChart style={styles.chart}
+                 chartData={chartData}
+                 verticalGridStep={5}
+                 xLabels={xLabels}
+             />
+        </View>
+    );
+  }
+
+  /* Render the image */
+/*  renderImage(): ReactElement {
+    return (
+      <Animated.Image                         // Base: Image, Text, View
+        source={{uri: 'http://i.imgur.com/XMKOH81.jpg'}}
+        style={styles.image}
+      />
+    );
+  } */
 }
 
 AppRegistry.registerComponent('dashboard', () => dashboard);
